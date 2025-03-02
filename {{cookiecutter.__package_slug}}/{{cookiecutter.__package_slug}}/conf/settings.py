de0e1846c46d889f7605b4d9cd0145dbaf749cb8
Robert Hafner <tedivm@tedivm.com>
2025-03-02 13:55:15 -0600

    Fix pydantic parent class import

--- {{cookiecutter.__package_slug}}/{{cookiecutter.__package_slug}}/conf/settings.py
+++ {{cookiecutter.__package_slug}}/{{cookiecutter.__package_slug}}/conf/settings.py
@@ -1,14 +1,14 @@
-{%- if cookiecutter.include_quasiqueue == "n" and  cookiecutter.include_sqlalchemy == "n"%}
+{%- if cookiecutter.include_quasiqueue.lower() != "y" and  cookiecutter.include_sqlalchemy.lower() != "y"%}
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
 {%- else %}
 {%- set settings_classes = [] %}
-{%- if cookiecutter.include_quasiqueue == "y" %}
+{%- if cookiecutter.include_quasiqueue.lower() == "y" %}
 from quasiqueue import Settings as QuasiQueueSettings
 {{- settings_classes.append("QuasiQueueSettings") or "" }}
 {% endif %}
-{%- if cookiecutter.include_sqlalchemy == "y" %}
+{%- if cookiecutter.include_sqlalchemy.lower() == "y" %}
 from .db import DatabaseSettings
 {{- settings_classes.append("DatabaseSettings") or "" }}
 {% endif %}
